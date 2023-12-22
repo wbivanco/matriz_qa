ff998f272b2bf94ffb34fb5d606fab8a2cff0303
Walter Ruben Bivanco <wbivanco@practia.global>
2023-12-22 12:51:12 -0300

    Mejoras de los notebooks y scripts

--- libs/convert_excel_to_csv.py
+++ libs/convert_excel_to_csv.py
@@ -1,8 +1,25 @@
 import pandas as pd 
 
+""" Modo de uso:
+from libs.convert_excel_to_csv import convert_xlsx_to_csv
 
-def convert_xlsx_to_csv(file, sheet_name, csv_name, row1, row2, col1, col2):
-    """Convertir un archivo excel a csv"""
+convert_xlsx_to_csv('ruta_al_archivo.xlsx', 'nombre_hoja', 'nombre_columa1', 'nombre_columa1', 'ruta_al_archivo.csv', numero_fila1, numero_fila2)
+"""
+
+
+def convert_xlsx_to_csv(file, sheet_name, col1, col2, csv_name='nombre_por_defecto', row1=0, row2=300):
+    """Convertir un archivo excel a csv"""'Nombre de la hoja', 
+
+    # Validaciones varias para campos obligatorios por la lógica del programa.
+    if file is None:
+        raise ValueError('Debe enviar un archivo válido')
+    
+    if sheet_name is None:
+        raise ValueError('Debe enviar un nombre de hoja válido')
+    
+    if col1 is None and col2 is None:
+        raise ValueError('Debe enviar dos nombres de columna válidos') 
+    
     df = pd.read_excel(file, sheet_name=sheet_name)
 
     subset1 = df.iloc[row1:row2]
--- libs/docx_parser.py
+++ libs/docx_parser.py
@@ -3,25 +3,39 @@ import re
 import csv
 
 
+def validate_file(filename):
+    """Validates if the file is a docx or csv file and if filename is not empty"""
+    if filename is None:
+        raise ValueError('Debe enviar un archivo válido')
+
+    if not filename.endswith('.docx') and not filename.endswith('.csv'):
+         raise ValueError('Debe enviar un archivo con extensión .docx o .csv')
+    
+    return True
+
+
 def getText(filename):
     """Returns the text of a docx file, with extra lines"""
-    doc = docx.Document(filename)
-    fullText = []
-    for para in doc.paragraphs:
-        fullText.append(para.text)
-    return '\n'.join(fullText)
+    if validate_file(filename):
+        doc = docx.Document(filename)
+        fullText = []
+        for para in doc.paragraphs:
+            fullText.append(para.text)
+        return '\n'.join(fullText)
 
 
 def getDoc(filename):
     """Returns the text of a docx file, with no extra lines"""
-    text=getText(filename)
-    textmod=re.sub(r'\n+', '\n', text)
-    return textmod
+    if validate_file(filename):
+        text=getText(filename)
+        textmod=re.sub(r'\n+', '\n', text)
+        return textmod
 
 
 def getCsv(filename):
     """Returns the text of a csv file, with no extra lines"""
-    with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        csv_content = '\n'.join(','.join(row) for row in reader)
-    return csv_content    
+    if validate_file(filename):
+        with open(filename, 'r') as file:
+            reader = csv.reader(file)
+            csv_content = '\n'.join(','.join(row) for row in reader)
+        return csv_content    
